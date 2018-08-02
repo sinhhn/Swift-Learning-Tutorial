77442bc72255ae5bf26f4ed0b494521c32b76284
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-08-02 12:49:42 +0900

    add commit for dice app

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+
+    @IBOutlet weak var diceimageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +26,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+    }
 
 }
 
