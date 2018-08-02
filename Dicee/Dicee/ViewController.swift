5266ab6aa51df8d34b34da879d78aaf922c13b49
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-08-02 13:46:11 +0900

    add commit for dice app

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
-
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceimageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -30,6 +32,9 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: Any) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceimageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
 
 }
