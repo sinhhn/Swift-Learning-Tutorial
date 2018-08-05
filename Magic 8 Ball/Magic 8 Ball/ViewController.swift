8d646ca0609384a07a7915c21a41e11329a539d9
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-08-06 07:13:45 +0900

    complete magic 8 ball chalenge

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomIndex: Int = 0
+    let imageArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBallImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        updateBallImages()
+    }
+    
+    func updateBallImages() {
+        randomIndex = Int(arc4random_uniform(5))
+        ballImageView.image = UIImage(named: imageArray[randomIndex])
+    }
 }
 
