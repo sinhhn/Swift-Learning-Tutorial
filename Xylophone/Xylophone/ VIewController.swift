ae927890863f14defab2a5c354321e445aa04b69
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-08-22 08:09:34 +0900

    add audio app

--- Xylophone/Xylophone/ VIewController.swift
+++ Xylophone/Xylophone/ VIewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,11 +21,35 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        let soundUrl = Bundle.main.url(forResource: getSound(sender), withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
         
     }
     
-  
+    func getSound(_ sender: UIButton) -> String {
+        switch sender.tag {
+        case 1:
+            return "note1"
+        case 2:
+            return "note2"
+        case 3:
+            return "note3"
+        case 4:
+            return "note4"
+        case 5:
+            return "note5"
+        case 6:
+            return "note6"
+        default:
+            return ""
+        }
+    }
 
 }
 
