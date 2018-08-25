d5273d29b4d9ee180f11aa6daa3af64ae609899a
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-08-26 06:52:17 +0900

    Refactor for encapsulating

--- Xylophone/Xylophone/ VIewController.swift
+++ Xylophone/Xylophone/ VIewController.swift
@@ -9,19 +9,23 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
-    
+
     var audioPlayer: AVAudioPlayer!
     typealias SoundFile = (fileName: String, extension: String)
+    let audioFiles = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    
+
+
+
     @IBAction func notePressed(_ sender: UIButton) {
-        let soundFile: SoundFile = getSound(sender)
-        
-        let soundUrl = Bundle.main.url(forResource: soundFile.fileName, withExtension: soundFile.extension)
+        playSound(audioFiles[sender.tag - 1], "wav")
+    }
+
+    func playSound(_ fileName: String, _ extention: String) {
+        let soundUrl = Bundle.main.url(forResource: fileName, withExtension: extention)
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
         }
@@ -29,28 +33,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
             print(error)
         }
         audioPlayer.play()
-        
     }
-    
-    func getSound(_ sender: UIButton) -> SoundFile {
-        let type = "wav"
-        switch sender.tag {
-        case 1:
-            return ("note1", type)
-        case 2:
-            return ("note2", type)
-        case 3:
-            return ("note3", type)
-        case 4:
-            return ("note4", type)
-        case 5:
-            return ("note5", type)
-        case 6:
-            return ("note6", type)
-        default:
-            return ("", "")
-        }
-    }
-    
-}
 
+}
